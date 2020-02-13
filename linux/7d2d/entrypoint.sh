262cb26608e2ed191ee1fe0b270de7995daba717
STAM <developer@epicm.org>
2020-02-13 17:33:44 +0300

    7d2d container build fix

--- linux/7d2d/entrypoint.sh
+++ linux/7d2d/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-$VALVE_GAME_FOLDER/7DaysToDieServer.x86_64 \
+$VALVE_GAME_FOLDER/startserver.sh \
 -logfile $VALVE_VOLUME_FOLDER/7d2d_output_log__`date +%Y-%m-%d__%H-%M-%S`.txt \ 
 -quit -batchmode -nographics -dedicated -configfile=$VALVE_VOLUME_FOLDER/serverconfig.xml
