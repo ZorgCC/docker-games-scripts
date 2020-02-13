0f666997a5f0e8cacb9324e572b73501c93a659b
Guahos <chipmunkzombie@gmail.com>
2020-02-13 13:41:41 +0000

    7d2d

--- linux/7d2d/run_7d2d.sh
+++ linux/7d2d/run_7d2d.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 $VALVE_GAME_FOLDER/7DaysToDieServer.x86_64 \
--logfile $VALVE_STEAMCMD_FOLDER/vol/7d2d_output_log__`date +%Y-%m-%d__%H-%M-%S`.txt \ 
--quit -batchmode -nographics -dedicated -configfile=$VALVE_STEAMCMD_FOLDER/vol/serverconfig.xml
+-logfile $VALVE_VOLUME_FOLDER/7d2d_output_log__`date +%Y-%m-%d__%H-%M-%S`.txt \ 
+-quit -batchmode -nographics -dedicated -configfile=$VALVE_VOLUME_FOLDER/serverconfig.xml
