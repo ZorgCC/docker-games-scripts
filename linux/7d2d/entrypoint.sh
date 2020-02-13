43b5e07fd4630ebd80340d095115cc9004179a11
STAM <developer@epicm.org>
2020-02-13 17:57:15 +0300

    fix of 7d2d

--- linux/7d2d/entrypoint.sh
+++ linux/7d2d/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-$VALVE_GAME_FOLDER/startserver.sh \
--logfile $VALVE_VOLUME_FOLDER/7d2d_output_log__`date +%Y-%m-%d__%H-%M-%S`.txt \ 
--quit -batchmode -nographics -dedicated -configfile=$VALVE_VOLUME_FOLDER/serverconfig.xml
+
+echo ${VALVE_GAME_FOLDER}
+echo ${VALVE_VOLUME_FOLDER}
+
+ls -las ${VALVE_GAME_FOLDER}
+ls -las ${VALVE_VOLUME_FOLDER}
+
+${VALVE_GAME_FOLDER}/7DaysToDieServer.x86_64 -logfile ${VALVE_VOLUME_FOLDER}/7d2d_output_log__`date +%Y-%m-%d__%H-%M-%S`.txt -quit -batchmode -nographics -dedicated -configfile=${VALVE_VOLUME_FOLDER}/serverconfig.xml
