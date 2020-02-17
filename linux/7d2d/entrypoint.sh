ddf4da3ab614aee548b2fcbc3052914666995ed0
Guahos <chipmunkzombie@gmail.com>
2020-02-17 13:03:42 +0000

    7d2d_ds_fix

--- linux/7d2d/entrypoint.sh
+++ linux/7d2d/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 $VALVE_GAME_FOLDER/7DaysToDieServer.x86_64 \
-    -logfile $VALVE_VOLUME_FOLDER/7d2d_output_log__`date +%Y-%m-%d__%H-%M-%S`.txt \
-	-quit -batchmode -nographics -dedicated \
-	-configfile=$VALVE_VOLUME_FOLDER/serverconfig.xml
+    -logfile $VALVE_VOLUME_FOLDER/7d2d_output_`date +%Y-%m-%d_%H-%M`.log \
+        -quit -batchmode -nographics -dedicated \
+        -configfile=$VALVE_VOLUME_FOLDER/serverconfig.xml
+
